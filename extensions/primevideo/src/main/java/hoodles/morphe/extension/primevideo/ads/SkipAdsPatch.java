f90cb6e199aba2aaa9bdc2900e242bb76ca37efd
KikuToshi <tomonii19920123@gmail.com>
2026-09-23 06:09:58 +0900

    Add files via upload

--- extensions/primevideo/src/main/java/hoodles/morphe/extension/primevideo/ads/SkipAdsPatch.java
+++ extensions/primevideo/src/main/java/hoodles/morphe/extension/primevideo/ads/SkipAdsPatch.java
@@ -73,7 +73,7 @@ public final class SkipAdsPatch {
                                 )
                         );
 
-                Thread.sleep(46)
+                        Thread.sleep(46);
 
 /*
                         Logger.printDebug(() ->
