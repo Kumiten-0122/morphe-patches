53e64aee07ed4c8b63d6f8d95b053c9e2e071708
KikuToshi <tomonii19920123@gmail.com>
2026-09-22 23:58:47 +0900

    Add files via upload

--- extensions/primevideo/src/main/java/hoodles/morphe/extension/primevideo/ads/SkipAdsPatch.java
+++ extensions/primevideo/src/main/java/hoodles/morphe/extension/primevideo/ads/SkipAdsPatch.java
@@ -49,9 +49,9 @@ public final class SkipAdsPatch {
 
             // Simulate rapid seek spam similar to repeatedly pressing seek buttons.
             long[] offsets = new long[] {
-                    93L,
-                    70L,
-                    45L,
+                    -93L,
+                    -70L,
+                    -45L,
                     0L
             };
 
