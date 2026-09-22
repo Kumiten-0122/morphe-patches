b1665d52c20a654677f8abed54eb98d5c494e86d
KikuToshi <tomonii19920123@gmail.com>
2026-09-23 08:59:51 +0900

    Add files via upload

--- extensions/primevideo/src/main/java/hoodles/morphe/extension/primevideo/ads/SkipAdsPatch.java
+++ extensions/primevideo/src/main/java/hoodles/morphe/extension/primevideo/ads/SkipAdsPatch.java
@@ -35,7 +35,7 @@ public final class SkipAdsPatch {
 
                 while (player.getCurrentPosition() <= targetPosition)
                     player.seekTo(targetPosition);
-                    Thread.sleep(46) 
+                    Thread.sleep(46);
             }
 
             // Send "end of ads" trigger to state machine so everything doesn't get wacky.
