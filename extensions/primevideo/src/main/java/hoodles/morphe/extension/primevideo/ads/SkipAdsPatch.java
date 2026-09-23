1924423de0d860a8b00e43f2504099de11dd270c
KikuToshi <tomonii19920123@gmail.com>
2026-09-23 22:22:38 +0900

    Add files via upload

--- extensions/primevideo/src/main/java/hoodles/morphe/extension/primevideo/ads/SkipAdsPatch.java
+++ extensions/primevideo/src/main/java/hoodles/morphe/extension/primevideo/ads/SkipAdsPatch.java
@@ -21,27 +21,29 @@ public final class SkipAdsPatch {
             AdBreak adBreak = trigger.getBreak();
 
             // There are two scenarios when entering the original method:
-            //  1. Player naturally entered an ad break while watching a video.
-            //  2. User is skipped/scrubbed to a position on the timeline. If seek position is past an ad break,
-            //     user is forced to watch an ad before continuing.
+            //   1. Player naturally entered an ad break while watching a video.
+            //   2. User is skipped/scrubbed to a position on the timeline. If seek position is past an ad break,
+            //      user is forced to watch an ad before continuing.
             //
             // Scenario 2 is indicated by trigger.getSeekStartPosition() != null, so skip directly to the scrubbing
             // target. Otherwise, just calculate when the ad break should end and skip to there.
-            if (trigger.getSeekStartPosition() != null)
+            if (trigger.getSeekStartPosition() != null) {
+                
                 player.seekTo(trigger.getSeekTarget().getTotalMilliseconds());
-            else {
-                long targetPosition = player.getCurrentPosition()
-                        + adBreak.getDurationExcludingAux().getTotalMilliseconds() - 1000;
+           
+           } else if (adBreak != null) {
 
-                while (player.getCurrentPosition() < targetPosition) {
-                    Thread.sleep(460);
-                    player.seekTo(targetPosition);
-                    Logger.printInfo(() -> "[SkipAds] player.getCurrentPosition() = "  + player.getCurrentPosition() + "   targetPosition = " + targetPosition);
-                }
-            }
+                long targetPosition = player.getCurrentPosition()
+                        + adBreak.getDurationExcludingAux().getTotalMilliseconds();
+
+                player.seekTo(targetPosition);
+                Logger.printInfo(() -> "[SkipAds] player.getCurrentPosition() = " + player.getCurrentPosition() + "   targetPosition = " + targetPosition);
+           
+           }
 
             // Send "end of ads" trigger to state machine so everything doesn't get wacky.
             state.doTrigger(new SimpleTrigger(AdEnabledPlayerTriggerType.NO_MORE_ADS_SKIP_TRANSITION));
+            
         } catch (Exception ex) {
             Logger.printException(() -> "Failed skipping ads", ex);
         }
