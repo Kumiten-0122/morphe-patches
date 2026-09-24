0cc04e4d8522a515f0e9a099836989a189715025
KikuToshi <tomonii19920123@gmail.com>
2026-09-24 11:06:10 +0900

    Add files via upload

--- extensions/primevideo/src/main/java/hoodles/morphe/extension/primevideo/ads/SkipAdsPatch.java
+++ extensions/primevideo/src/main/java/hoodles/morphe/extension/primevideo/ads/SkipAdsPatch.java
@@ -31,7 +31,7 @@ public final class SkipAdsPatch {
                 player.seekTo(trigger.getSeekTarget().getTotalMilliseconds());
             else {
                 long targetPosition = player.getCurrentPosition()
-                        + adBreak.getDurationExcludingAux().getTotalMilliseconds()
+                        + adBreak.getDurationExcludingAux().getTotalMilliseconds();
 
                 player.seekTo(0);
 
