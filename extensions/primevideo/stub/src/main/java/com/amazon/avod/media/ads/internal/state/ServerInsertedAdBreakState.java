aaa83c7661587d70f2ca8fd65d7311c8f6c47abf
KikuToshi <tomonii19920123@gmail.com>
2026-09-26 01:26:47 +0900

    Update ServerInsertedAdBreakState.java

--- extensions/primevideo/stub/src/main/java/com/amazon/avod/media/ads/internal/state/ServerInsertedAdBreakState.java
+++ extensions/primevideo/stub/src/main/java/com/amazon/avod/media/ads/internal/state/ServerInsertedAdBreakState.java
@@ -4,9 +4,8 @@ import com.amazon.avod.fsm.Trigger;
 import com.amazon.avod.media.playback.state.trigger.PlayerTriggerType;
 
 public class ServerInsertedAdBreakState extends AdBreakState {
-     @Override // com.amazon.avod.media.ads.internal.state.AdEnabledPlaybackState, com.amazon.avod.fsm.StateBase, com.amazon.avod.fsm.State
-     public void exit(Trigger<PlayerTriggerType> trigger) {
 
+     public void exit(Trigger<PlayerTriggerType> trigger) {
     }
 
 }
