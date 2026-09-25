672472d8f43bcc33ea8dec79aba7138957752342
KikuToshi <tomonii19920123@gmail.com>
2026-09-25 11:19:41 +0900

    Update AdBreakBufferContext.java

--- extensions/primevideo/stub/src/main/java/com/amazon/avod/media/ads/internal/state/AdBreakBufferContext.java
+++ extensions/primevideo/stub/src/main/java/com/amazon/avod/media/ads/internal/state/AdBreakBufferContext.java
@@ -2,7 +2,6 @@ package com.amazon.avod.media.ads.internal.state;
 
 import com.amazon.avod.threading.Tickers;
 import com.google.common.base.Stopwatch;
-import javax.annotation.concurrent.NotThreadSafe;
 
 public class AdBreakBufferContext {
     private int mCurrentBreakBuffers = 0;
