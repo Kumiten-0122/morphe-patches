1c0190668d352272c17c63200ac0b749c8ee3153
KikuToshi <tomonii19920123@gmail.com>
2026-09-25 18:22:06 +0900

    Update SkipAdsPatch.java

--- extensions/primevideo/src/main/java/hoodles/morphe/extension/primevideo/ads/SkipAdsPatch.java
+++ extensions/primevideo/src/main/java/hoodles/morphe/extension/primevideo/ads/SkipAdsPatch.java
@@ -60,7 +60,7 @@ public final class SkipAdsPatch {
                         }
 
                         // 条件を満たすまで監視を継続
-                        handler.postDelayed(this, 460);
+                        handler.postDelayed(this, 1046);
                     }
                 };
 
