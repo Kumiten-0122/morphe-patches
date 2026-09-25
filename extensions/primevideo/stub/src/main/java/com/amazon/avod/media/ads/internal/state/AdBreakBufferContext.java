b8555f96b4c3b58ec18ca1c4b1fb5ba5c97c24ca
KikuToshi <tomonii19920123@gmail.com>
2026-09-25 11:33:32 +0900

    Update AdBreakBufferContext.java

--- extensions/primevideo/stub/src/main/java/com/amazon/avod/media/ads/internal/state/AdBreakBufferContext.java
+++ extensions/primevideo/stub/src/main/java/com/amazon/avod/media/ads/internal/state/AdBreakBufferContext.java
@@ -1,18 +1,8 @@
 package com.amazon.avod.media.ads.internal.state;
 
 public class AdBreakBufferContext {
-    private int mCurrentBreakBuffers = 0;
-
-    private void clearCurrentBreakBuffers() {
-        this.mCurrentBreakBuffers = 0;
-    }
-
-    private void resetBufferStopwatch() {
-        this.mBufferStopwatch.reset();
-    }
 
     public void reset() {
-        clearCurrentBreakBuffers();
-        resetBufferStopwatch();
+        throw new UnsupportedOperationException();
     }
 }
