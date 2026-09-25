89e60a77623895d5a851d1f8957c85e8975d057b
KikuToshi <tomonii19920123@gmail.com>
2026-09-25 18:07:06 +0900

    Update SkipAdsPatch.java

--- extensions/primevideo/src/main/java/hoodles/morphe/extension/primevideo/ads/SkipAdsPatch.java
+++ extensions/primevideo/src/main/java/hoodles/morphe/extension/primevideo/ads/SkipAdsPatch.java
@@ -52,7 +52,7 @@ public final class SkipAdsPatch {
                         if (currentPosition > targetPosition + 100) {
 
                             player.pause();
-                            player.start();
+                            //player.start();
 
                             // 監視終了
                             handler.removeCallbacks(this);
