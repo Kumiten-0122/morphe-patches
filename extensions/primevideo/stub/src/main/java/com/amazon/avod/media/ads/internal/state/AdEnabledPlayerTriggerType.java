ba17654f975a97e81c2566452ecbdb8be4cdbd90
KikuToshi <tomonii19920123@gmail.com>
2026-09-25 20:53:40 +0900

    Update AdEnabledPlayerTriggerType.java

--- extensions/primevideo/stub/src/main/java/com/amazon/avod/media/ads/internal/state/AdEnabledPlayerTriggerType.java
+++ extensions/primevideo/stub/src/main/java/com/amazon/avod/media/ads/internal/state/AdEnabledPlayerTriggerType.java
@@ -2,5 +2,6 @@ package com.amazon.avod.media.ads.internal.state;
 
 public enum AdEnabledPlayerTriggerType {
     NO_MORE_ADS_SKIP_TRANSITION,
+    SKIP_CURRENT_AD_CLIP,
     NEXT_AD_CLIP_SERVER_INSERTED
 }
