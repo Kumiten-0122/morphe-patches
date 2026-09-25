44607a6c25c356e790c4dd0dcfc415b5950dd9ee
KikuToshi <tomonii19920123@gmail.com>
2026-09-25 09:47:54 +0900

    Update SkipAdsPatch.java

--- extensions/primevideo/src/main/java/hoodles/morphe/extension/primevideo/ads/SkipAdsPatch.java
+++ extensions/primevideo/src/main/java/hoodles/morphe/extension/primevideo/ads/SkipAdsPatch.java
@@ -42,7 +42,7 @@ public final class SkipAdsPatch {
             }
 
             // Send "end of ads" trigger to state machine so everything doesn't get wacky.
-            state.doTrigger(new SimpleTrigger(AdEnabledPlayerTriggerType.NO_MORE_ADS_SKIP_TRANSITION));
+            state.doTrigger(new SimpleTrigger(AdEnabledPlayerTriggerType.NO_MORE_ADS));
         } catch (Exception ex) {
             Logger.printException(() -> "Failed skipping ads", ex);
         }
