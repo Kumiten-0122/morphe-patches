bc7efea14f8c07fa4679f1ebc4de15a3d8e677fc
KikuToshi <tomonii19920123@gmail.com>
2026-09-27 00:56:06 +0900

    Update SkipAdsPatch.java

--- extensions/primevideo/src/main/java/hoodles/morphe/extension/primevideo/ads/SkipAdsPatch.java
+++ extensions/primevideo/src/main/java/hoodles/morphe/extension/primevideo/ads/SkipAdsPatch.java
@@ -46,9 +46,9 @@ public final class SkipAdsPatch {
                 AmazonVideoPlayer.seekTo(targetPosition);
 
                 Logger.printInfo(() -> "[SkipAds] player.getCurrentPosition() = "  + player.getCurrentPosition() + "   targetPosition = " + targetPosition);
-
-                Handler handler = new Handler(Looper.getMainLooper());
 /*
+                Handler handler = new Handler(Looper.getMainLooper());
+
                 Runnable checkPlayerPosition = new Runnable() {
                     @Override
                     public void run() {
@@ -78,9 +78,9 @@ public final class SkipAdsPatch {
 
                 // 監視開始
                 handler.post(checkPlayerPosition); 
-
-            }
 */
+            }
+
             // Send "end of ads" trigger to state machine so everything doesn't get wacky.
             state.doTrigger(new SimpleTrigger(AdEnabledPlayerTriggerType.NO_MORE_ADS_SKIP_TRANSITION));
             state.exit(new SimpleTrigger(AdEnabledPlayerTriggerType.NO_MORE_ADS_SKIP_TRANSITION));
