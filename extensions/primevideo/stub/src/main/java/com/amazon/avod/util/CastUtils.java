70750278b53589f029382c61f8fd86fc1ff8164c
KikuToshi <tomonii19920123@gmail.com>
2026-09-27 00:24:18 +0900

    Update CastUtils.java

--- extensions/primevideo/stub/src/main/java/com/amazon/avod/util/CastUtils.java
+++ extensions/primevideo/stub/src/main/java/com/amazon/avod/util/CastUtils.java
@@ -2,5 +2,10 @@ package com.amazon.avod.util;
 
 public final class CastUtils {
     public static <T> T castTo(Object obj, Class<T> cls) {
+        if (cls.isInstance(obj)) {
+            return cls.cast(obj);
+        }
+        return null;
+
     }
 }
