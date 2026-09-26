0a35761a4a4357a98c701c1e19e76896747f5538
KikuToshi <tomonii19920123@gmail.com>
2026-09-26 23:15:51 +0900

    Update AmazonVideoPlayer.java

--- extensions/primevideo/stub/src/main/java/com/amazon/avod/playback/session/AmazonVideoPlayer.java
+++ extensions/primevideo/stub/src/main/java/com/amazon/avod/playback/session/AmazonVideoPlayer.java
@@ -1,4 +1,7 @@
 package com.amazon.avod.playback.session;
 
+import com.amazon.avod.playback.PlaybackExperienceController;
+import com.amazon.avod.playback.PlaybackExperienceController;
+
 public class AmazonVideoPlayer extends VideoPlayerBase implements PlaybackExperienceController {
 }
